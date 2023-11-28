155aa8752eb6c4f80af4fb506b0dfa6d5c669cc5
RimjhimD <129301857+RimjhimD@users.noreply.github.com>
2023-11-28 19:36:04 +0600

    OOP lab 1.java

--- OOP lab 1.java
+++ OOP lab 1.java
@@ -1,5 +1,6 @@
 public class Calculator {
-    int numberOne,numberTwo;
+    int numberOne;
+    int numberTwo;
     Calculator(int numberOne, int numberTwo)
     {
         this.numberOne = numberOne;
@@ -8,11 +9,15 @@ public class Calculator {
     void add()
     { System.out.println(numberOne+numberTwo);          
     }
+    void sub()
+    { System.out.println(numberOne-numberTwo);
+    }
  
     public static void main(String[] args)
     {
         Calculator c1 = new Calculator(3,4);
         c1.add();
+        c1.sub();
     
    }
 }
