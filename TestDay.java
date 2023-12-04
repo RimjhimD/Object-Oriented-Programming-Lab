54e3e2f642d7b0449a98666e1dd5c2f7fe654ab9
Rii <129301857+RimjhimD@users.noreply.github.com>
2023-12-04 15:51:44 +0600

    Delete TestDay.java

--- TestDay.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Codechef {
-  public static void main(String[] args) {
-    System.out.print("Hello");
-    System.out.println();
-    System.out.print("World");
-  }
-}
