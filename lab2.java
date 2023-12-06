23f7ab82745502805cbb3fabc0419a72f2abf23d
Rii <129301857+RimjhimD@users.noreply.github.com>
2023-12-06 13:25:50 +0600

    Lab 2

--- lab2.java
+++ lab2.java
@@ -1,37 +1,42 @@
+
 import java.util.Scanner;
 
 public class Calculator {
     int numberOne;
     int numberTwo;
 
-    static Scanner sc = new Scanner(System.in);
-
-    Calculator(int numberOne, int numberTwo) {
+    Calculator(int numberOne, int numberTwo)
+    {
         this.numberOne = numberOne;
         this.numberTwo = numberTwo;
     }
-
-    void add() {
-        System.out.println("Sum: " + (numberOne + numberTwo));
+    void add()
+    { System.out.println("sum: " + (numberOne+numberTwo));          
     }
-
-    void sub() {
-        System.out.println("Difference: " + (numberOne - numberTwo));
+    void sub()
+    { System.out.println("Difference: " + (numberOne-numberTwo));
     }
-
-    public static void main(String[] args) {
+ 
+    public static void main(String[] args)
+    {
         // Taking input for numberOne and numberTwo
-        System.out.print("Enter the first number: ");
-        int inputOne = sc.nextInt();
+         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the second number: ");
-        int inputTwo = sc.nextInt();
+         System.out.print("Enter the first number: ");
+         int inputOne = sc.nextInt();
 
-        // Creating Calculator object with user input
+         System.out.print("Enter the second number: ");
+         int inputTwo = sc.nextInt();
+
+         // Creating Calculator object with user input
         Calculator c1 = new Calculator(inputOne, inputTwo);
 
         // Calling add and sub methods
         c1.add();
         c1.sub();
-    }
+
+        // Closing the scanner to prevent resource leak
+        sc.close();
+    
+   }
 }
