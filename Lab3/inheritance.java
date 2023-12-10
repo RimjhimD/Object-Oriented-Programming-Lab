8bac45178cc99eac9dda9d097da97f37a2dfdb8a
Rii <129301857+RimjhimD@users.noreply.github.com>
2023-12-10 14:14:48 +0600

    Inheritance

---
+++ Lab3/inheritance.java
@@ -0,0 +1,27 @@
+class Bicycle {
+
+    public int gear;
+    public int speed;
+}
+
+class Motorbike extends Bicycle {
+
+
+    public int bikeHeight;
+
+
+    public Motorbike(int gear,int speed,int seatHeight);
+    {
+
+       super(gear,speed);
+       int bikeHeight = int seatHeight;
+
+
+
+    }
+
+
+
+
+
+}
